86deda811cce06eb7f05845bf300851f20b2461a
yoanp <pouliart.yoan@gmail.com>
2025-03-26 21:39:54 +0100

    Create Player class and render on screen

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    #Init pygame
     pygame.init()
+
+    #Init game screen size
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    #Initialize delta time and FPS
+    clock = pygame.time.Clock()
+    dt = 0
 
     while(True):
         for event in pygame.event.get():
@@ -15,7 +23,16 @@ def main():
                 return
             
         pygame.Surface.fill(screen, color=(0,0,0))
+        
+        #Player render
+        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        player.draw(screen)
+        
         pygame.display.flip()
+
+        #Tick clock 60 fps
+        ms = clock.tick(60)
+        dt = ms / 1000
 
 if __name__ == "__main__":
     main()
